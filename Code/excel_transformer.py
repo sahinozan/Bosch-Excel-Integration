a253a88b422f75f459cab0f1221df76fd8479814
Ozan Şahin <ozansahin.ac@gmail.com>
2022-12-26 13:27:36 +0300

    Added import validation

--- Code/excel_transformer.py
+++ Code/excel_transformer.py
@@ -1,27 +1,53 @@
-import pandas as pd
+from __future__ import annotations
+from importlib.util import find_spec
+import subprocess
 import datetime
+import sys
+
+if sys.version_info.major != 3:
+    raise SystemError("!!> Python version must be 3.X.X (preferably, 3.11.X)")
+if sys.version_info.minor != 11:
+    print("??> This script is written for Python 3.11.X, "
+          "It may not work properly with other versions.\n"
+          "??> Do you still want to continue? (Y/N)")
+    user_input = input(">>> ")
+    if user_input == 'Y':
+        pass
+    elif user_input == 'N':
+        print(">>> Terminating...")
+        exit(0)
+    else:
+        raise SystemError("!!> Invalid input!")
+
+if find_spec('pandas') is None:
+    print("\n>>> Installing pandas...\n")
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas', '--disable-pip-version-check'])
+if find_spec("openpyxl") is None:
+    print("\n>>> Installing openpyxl...\n")
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'openpyxl', '--disable-pip-version-check'])
+    
+import pandas as pd
 
 try:
-    file = pd.read_excel('Data/KW47_V00.xlsx')
-    pipes = pd.read_excel('Data/Cihazlar - Borular.xlsx')
+    file = pd.read_excel('../Data/KW47_V00.xlsx')
+    pipes = pd.read_excel('../Data/Cihazlar - Borular.xlsx')
 except FileNotFoundError:
     print("File not found!")
     exit(1)
 
 indices = file.iloc[:, [0, 7, 8, 11]].reset_index()
-days = file.iloc[:, 12: 33].reset_index()
-combined = pd.concat([indices, days], axis=1).iloc[2:, :]
+work_days = file.iloc[:, 12: 33].reset_index()
+sheet = pd.concat([indices, work_days], axis=1).iloc[2:, :]
 
-combined = combined[combined.iloc[:, 1].notna()]
-combined = combined[combined.iloc[:, 2].notna()]
-combined.iloc[:, 2] = combined.iloc[:, 2].astype(str)
-combined = combined[combined.iloc[:, 2].apply(str.isnumeric)]
+sheet = sheet[sheet.iloc[:, 1].notna() & sheet.iloc[:, 2].notna()]
+sheet.iloc[:, 2] = sheet.iloc[:, 2].astype(str)
+sheet = sheet[sheet.iloc[:, 2].apply(str.isnumeric)]
 
-combined = combined[combined.iloc[:, 6].apply(lambda x: (type(x) != datetime.datetime) and (type(x) != str))]
-combined = combined[combined.iloc[:, 3].notna()]
+sheet = sheet[sheet.iloc[:, 6].apply(lambda x: (type(x) != datetime.datetime) and (type(x) != str))]
+sheet = sheet[sheet.iloc[:, 3].notna()]
 
-combined.drop('index', axis=1, inplace=True)
-combined.reset_index(drop=True, inplace=True)
+sheet.drop('index', axis=1, inplace=True)
+sheet.reset_index(drop=True, inplace=True)
 
 initial_indices = ["Hat", "Cihaz TTNr", "Cihaz Aile", "Toplam Adet"]
 
@@ -31,25 +57,25 @@ shifts = ["1", "2", "3"]
 final_indices = [" ".join([i, j]) for i in week_days for j in shifts]
 initial_indices.extend(final_indices)
 
-combined = combined.set_axis(initial_indices, axis=1)
-combined.set_index("Hat")
+sheet = sheet.set_axis(initial_indices, axis=1)
+sheet.set_index("Hat")
 
-combined["Cihaz TTNr"] = combined["Cihaz TTNr"].astype(str)
+sheet["Cihaz TTNr"] = sheet["Cihaz TTNr"].astype(str)
 pipes["Cihaz"] = pipes["Cihaz"].astype(str)
 
-combined = combined.merge(pipes, left_on="Cihaz TTNr", right_on="Cihaz", how="inner")
-combined.drop("Cihaz", axis=1, inplace=True)
-combined.insert(3, 'Boru', combined.pop('Boru'))
+sheet = sheet.merge(pipes, left_on="Cihaz TTNr", right_on="Cihaz", how="inner")
+sheet.drop("Cihaz", axis=1, inplace=True)
+sheet.insert(3, 'Boru', sheet.pop('Boru'))
 
-for i in range(combined.shape[0]):
-    combined.loc[i, "Pazartesi 1": "Pazar 3"] *= combined.loc[i, "Miktar"]
+for i in range(sheet.shape[0]):
+    sheet.loc[i, "Pazartesi 1": "Pazar 3"] *= sheet.loc[i, "Miktar"]
 
-combined.drop("Miktar", axis=1, inplace=True)
-combined.drop("Toplam Adet", axis=1, inplace=True)
-combined.set_index("Hat", inplace=True)
+sheet.drop("Miktar", axis=1, inplace=True)
+sheet.drop("Toplam Adet", axis=1, inplace=True)
+sheet.set_index("Hat", inplace=True)
 
 try:
-    combined.to_excel("Data/initial.xlsx")
-    print("Success!")
+    sheet.to_excel("../Data/initial.xlsx")
+    print("\n>>> Successfully converted to excel file!")
 except PermissionError:
-    print("Failed!")
+    print("\n>>> Failed to convert!")
